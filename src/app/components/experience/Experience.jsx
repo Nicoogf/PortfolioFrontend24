cee055d88d17baddd189684df9f49e6f32dbf893
Nicolas <nicolasgfalabella@gmail.com>
2024-04-10 14:00:20 -0300

    Dark mode

--- src/app/components/experience/Experience.jsx
+++ src/app/components/experience/Experience.jsx
@@ -1,7 +1,19 @@
 import Image from 'next/image'
 import React from 'react'
+import {useSelector , useDispatch } from "react-redux" ;
 
 const Experience = ( props ) => {
+
+  const state = useSelector((state) =>  state.nightMode)
+  const estadoDeMenu = state.menuVisible
+
+  const coloroOscuro = useSelector((coloroOscuro) => coloroOscuro.colorTheme)
+  const isLight = coloroOscuro.colorTheme
+
+  console.log( isLight )
+
+  const dispatch = useDispatch()
+
   return (
     <article className="flex flex-row items-center gap-x-4 ">
       
@@ -14,9 +26,9 @@ const Experience = ( props ) => {
     />
 
     <div>               
-      <h3 className="font-bold text-sm sm:text-md text-gray-200 mb-[1px]">{props.name} </h3>
-      <h5 className="text-gray-400 text-xs sm:text-sm">{props.rol}</h5>
-      <h6 className="text-xs sm:text-sm text-gray-400"> {props.date} </h6>
+      <h3 className={` ${ isLight ? "text-gray-800": "text-gray-200" } font-bold text-sm sm:text-md  mb-[1px]`}>{props.name} </h3>
+      <h5 className={` ${ isLight ? "text-gray-800": "text-gray-200" } text-gray-400 text-xs sm:text-sm `} >{props.rol}</h5>
+      <h6 className={` ${ isLight ? "text-gray-600": "text-gray-400" } text-xs sm:text-sm `}> {props.date} </h6>
     </div>
   </article>
   )
